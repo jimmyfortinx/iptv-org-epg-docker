1b472d6096a836bf1a8ccfb43b518b32b87e3047
Jimmy Fortin <jimmy.fortin@gmail.com>
2024-04-01 06:53:21 -0400

    fix: add delay on tvpassport

--- cron.js
+++ cron.js
@@ -1,19 +1,32 @@
 const { execSync } = require("child_process");
 var cron = require("node-cron");
 
-const sites = ["tvhebdo.com", "tvpassport.com"];
-const languages = ["fr", "en"];
+const sites = {
+  "tvhebdo.com": {
+    delay: 0,
+    days: 7,
+    languages: ["fr", "en"],
+  },
+  "tvpassport.com": {
+    delay: 1000,
+    days: 3,
+    languages: ["fr", "en"],
+  },
+};
 
 const execute = () => {
   console.log("Grabbing epg...");
 
-  for (const site of sites) {
+  for (const [site, { delay, days, languages }] of Object.entries(sites)) {
     for (const language of languages) {
       try {
-        execSync(`npm run grab -- --site ${site} --lang ${language}`, {
-          stdio: "inherit",
-          cwd: "/usr/src/app",
-        });
+        execSync(
+          `npm run grab -- --site ${site} --lang ${language} --delay ${delay} --days ${days}`,
+          {
+            stdio: "inherit",
+            cwd: "/usr/src/app",
+          }
+        );
       } catch (error) {
         console.error(
           `something went wront while grabbing epg from ${site} in ${language}`,
